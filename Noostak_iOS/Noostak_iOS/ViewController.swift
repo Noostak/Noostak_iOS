7a1877ee0503c168f605adf0e365cf344d30cbc8
LeeMyeongJin <dlaudwls1203@gmail.com>
2024-12-28 17:01:35 +0900

    [Setting] - 코드베이스 프로젝트 초기 세팅 완료

---
+++ Noostak_iOS/Noostak_iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Noostak_iOS
+//
+//  Created by 이명진 on 12/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
