acae31dad74769bbd94c4c8b9641144cc50015ab
luckyyy <oyslucy@naver.com>
2025-01-06 14:40:46 +0900

    [Feat/#54] MemberAvailabilityChip

---
+++ Noostak_iOS/Noostak_iOS/Global/Components/MemberAvailabilityChip.swift
@@ -0,0 +1,82 @@
+//
+//  MemberAvailabilityChip.swift
+//  Noostak_iOS
+//
+//  Created by 오연서 on 1/6/25.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+enum MemberStatus {
+    case myself
+    case available
+    case unavailable
+}
+
+class MemberAvailabilityChip: UIView {
+    
+    private let chipLabel = UILabel()
+    private var status: MemberStatus = .myself
+    
+    init(name: String, status: MemberStatus) {
+        super.init(frame: .zero)
+        self.chipLabel.text = name
+        self.status = status
+        setUpHierarchy()
+        setUpUI()
+        setUpLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpHierarchy
+    private func setUpHierarchy() {
+        self.addSubview(chipLabel)
+    }
+    
+    // MARK: setUpUI
+    private func setUpUI() {
+        
+        self.do {
+            $0.layer.cornerRadius = 16
+            $0.layer.borderWidth = 1
+            
+            switch status {
+            case .myself:
+                $0.backgroundColor = .appBlue200
+                $0.layer.borderColor = UIColor.appBlue200.cgColor
+            case .available:
+                $0.backgroundColor = .appWhite
+                $0.layer.borderColor = UIColor.appBlue200.cgColor
+            case .unavailable:
+                $0.backgroundColor = .appGray200
+                $0.layer.borderColor = UIColor.appGray200.cgColor
+            }
+        }
+        
+        chipLabel.do {
+            $0.font = .PretendardStyle.c3_r.font
+            $0.textColor = .appBlack
+            $0.textAlignment = .center
+        }
+    }
+    
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        self.snp.makeConstraints {
+            if status == .myself {
+                $0.width.equalTo(28)
+            }
+            $0.height.equalTo(30)
+        }
+        chipLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(8)
+            $0.verticalEdges.equalToSuperview().inset(6)
+        }
+    }
+}
