2ec80749d32382af36d049b4a0575a1381db660e
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-07 03:07:39 +0900

    [Feat/#13] 텍스트 필드 공통 - PaddedTextField 작성

--- Noostak_iOS/Noostak_iOS/Global/PaddedTextField.swift
+++ Noostak_iOS/Noostak_iOS/Global/PaddedTextField.swift
@@ -5,4 +5,30 @@
 //  Created by 박민서 on 1/7/25.
 //
 
-import Foundation
+import UIKit
+
+final class PaddedTextField: UITextField {
+    private var padding: UIEdgeInsets
+
+    init(padding: UIEdgeInsets) {
+        self.padding = padding
+        super.init(frame: .zero)
+    }
+
+    required init?(coder: NSCoder) {
+        self.padding = .zero
+        super.init(coder: coder)
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
