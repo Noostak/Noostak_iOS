d8e19a7641db5ee95351baeac9fdbf1f751580b9
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-07 03:08:07 +0900

    [Feat/#13] AppTextFieldProtocol 작성

--- Noostak_iOS/Noostak_iOS/Global/AppTextFieldProtocol.swift
+++ Noostak_iOS/Noostak_iOS/Global/AppTextFieldProtocol.swift
@@ -5,4 +5,20 @@
 //  Created by 박민서 on 1/7/25.
 //
 
-import Foundation
+import UIKit
+import RxRelay
+
+protocol AppTextFieldProtocol: UIView {
+    /// 텍스트 필드 텍스트 상태
+    var textRelay: BehaviorRelay<String> { get }
+    /// 텍스트 필드 포커스 되었는지
+    var isFocused: Bool { get }
+    /// 텍스트 필드 포커스하기
+    /// true if this object is now the first responder; otherwise, false.
+    @discardableResult
+    func becomeFirstResponder() -> Bool
+    /// 텍스트 필드 포커스 해제하기
+    /// true if this object is now the first responder; otherwise, false.
+    @discardableResult
+    func resignFirstResponder() -> Bool
+}
