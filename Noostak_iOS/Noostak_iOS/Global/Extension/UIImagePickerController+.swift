f2e73841fb13f1db665f0ad484fd239828e37677
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-01-08 02:05:14 +0900

    [Feat/#15] UIImagePickerController, PHPickerViewController Reactive Extension 추가

--- Noostak_iOS/Noostak_iOS/Global/Extension/UIImagePickerController+.swift
+++ Noostak_iOS/Noostak_iOS/Global/Extension/UIImagePickerController+.swift
@@ -1,5 +1,5 @@
 //
-//  UIImagePickerController.swift
+//  UIImagePickerController+.swift
 //  Noostak_iOS
 //
 //  Created by 박민서 on 1/8/25.
@@ -9,7 +9,30 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+
 extension Reactive where Base: UIImagePickerController {
+    /// `UIImagePickerController`의 미디어 선택 결과를 Observable로 노출
+    ///
+    /// 이 Observable은 이미지를 선택하거나 취소한 후 결과를 반환합니다.
+    /// - Returns: 선택된 미디어 정보(`[UIImagePickerController.InfoKey: Any]`) 또는 완료 이벤트
+    ///
+    /// 사용 예시:
+    /// ```swift
+    /// let imagePicker = UIImagePickerController()
+    /// imagePicker.sourceType = .camera
+    ///
+    /// imagePicker.rx.didFinishPickingMedia
+    ///     .subscribe(onNext: { info in
+    ///         if let image = info[.editedImage] as? UIImage ?? info[.originalImage] as? UIImage {
+    ///             print("Picked image: \(image)")
+    ///         }
+    ///     }, onCompleted: {
+    ///         print("Picker dismissed.")
+    ///     })
+    ///     .disposed(by: disposeBag)
+    ///
+    /// present(imagePicker, animated: true)
+    /// ```
     public var didFinishPickingMedia: Observable<[UIImagePickerController.InfoKey: Any]> {
         return Observable.create { [weak base] observer in
             guard let picker = base else {
@@ -29,7 +52,8 @@ extension Reactive where Base: UIImagePickerController {
     }
 }
 
-class RxImagePickerDelegateProxy: DelegateProxy<UIImagePickerController, UIImagePickerControllerDelegate & UINavigationControllerDelegate>, DelegateProxyType {
+// MARK: - RxImagePickerDelegateProxy
+final class RxImagePickerDelegateProxy: DelegateProxy<UIImagePickerController, UIImagePickerControllerDelegate & UINavigationControllerDelegate>, DelegateProxyType {
     
     // PublishSubject를 통해 Delegate 호출을 Rx로 노출
     let didFinishPicking = PublishSubject<[UIImagePickerController.InfoKey: Any]>()
@@ -57,7 +81,7 @@ extension RxImagePickerDelegateProxy {
     }
 }
 
-// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate 구현
 extension RxImagePickerDelegateProxy: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         didFinishPicking.onNext(info)
