9ebb04c232a3e5609d80de86e1b8e4d73fbc4399
jaminleee <jamin_00@naver.com>
2025-02-26 21:29:24 +0900

    [Feat/#24] 코드 리뷰 반영

--- Noostak_iOS/Noostak_iOS/Global/Components/CapsuleButton.swift
+++ Noostak_iOS/Noostak_iOS/Global/Components/CapsuleButton.swift
@@ -14,25 +14,41 @@ import RxCocoa
 final class CapsuleButton: UIButton {
     
     // MARK: - Enum
-    enum CapsuleButtonType {
+    enum Style {
         case expand(isExpanded: Bool)
         case normal
+        
+        var title: String {
+            switch self {
+            case .expand(let isExpanded):
+                return isExpanded ? "" : "그룹 추가"
+            case .normal:
+                return "약속 만들기"
+            }
+        }
+        
+        var icon: UIImage? {
+            switch self {
+            case .expand(let isExpanded):
+                return isExpanded ? UIImage(systemName: "xmark") : UIImage(systemName: "plus")
+            case .normal:
+                return UIImage(systemName: "plus")
+            }
+        }
     }
     
     // MARK: - Properties
-    var capsuleButtonType: CapsuleButtonType
+    var style: Style
     private var action: (() -> Void)?
     private var widthConstraint: Constraint?
-    private let defaultTitle: String
-    
+
     // MARK: - Initializer
-    init(type: CapsuleButtonType, title: String, icon: UIImage?, action: (() -> Void)? = nil) {
-        self.defaultTitle = title
-        self.capsuleButtonType = type
+    init(style: Style, action: (() -> Void)? = nil) {
+        self.style = style
         self.action = action
         super.init(frame: .zero)
         setupUI()
-        configure(title: title, icon: icon)
+        configure()
         addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
@@ -44,55 +60,46 @@ final class CapsuleButton: UIButton {
     private func setupUI() {
         self.do {
             $0.backgroundColor = .appBlack
-            $0.layer.cornerRadius = 25
-        }
-        
-        self.snp.makeConstraints {
-            $0.height.equalTo(50)
-            self.widthConstraint = $0.width.equalTo(100).constraint
         }
     }
     
-    private func configure(title: String, icon: UIImage?) {
-        self.setTitle(title, for: .normal)
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            self.layer.cornerRadius = self.frame.height / 2
+        }
+    
+    private func configure() {
+        self.setTitle(style.title, for: .normal)
         self.setTitleColor(.appWhite, for: .normal)
         self.titleLabel?.font = .PretendardStyle.b4_sb.font
-        self.setImage(icon, for: .normal)
+        self.setImage(style.icon, for: .normal)
         self.tintColor = .appWhite
         self.imageView?.contentMode = .scaleAspectFit
     }
     
-    private func updateWidth(isExpanded: Bool) {
-
-    }
-    
     private func updateButton(isExpanded: Bool) {
-        let title = isExpanded ? "" : defaultTitle
-        let icon = isExpanded ? UIImage(systemName: "xmark") : UIImage(systemName: "plus")
-        configure(title: title, icon: icon)
+        style = .expand(isExpanded: isExpanded)
         
+        configure()
         self.backgroundColor = isExpanded ? .appBlue50 : .appBlack
         self.setTitleColor(isExpanded ? .appBlack : .appWhite, for: .normal)
         self.tintColor = isExpanded ? .appBlack : .appWhite
-        
-        let newWidth = isExpanded ? 50 : 100
-        self.widthConstraint?.update(offset: newWidth)
-        UIView.animate(withDuration: 0.3) {
-            self.superview?.layoutIfNeeded()
-        }
     }
     
     // MARK: - Actions
     @objc private func buttonTapped() {
-        switch capsuleButtonType {
+        switch style {
         case .expand(let isExpanded):
-            let newState = !isExpanded
-            capsuleButtonType = .expand(isExpanded: newState)
-            updateButton(isExpanded: newState)
+            updateButton(isExpanded: !isExpanded)
             action?()
         case .normal:
             action?()
         }
     }
+}
 
+#Preview {
+//    ViewController()
+    CapsuleButton(style: .expand(isExpanded: false))
+//    CapsuleButton(style: .normal)
 }
