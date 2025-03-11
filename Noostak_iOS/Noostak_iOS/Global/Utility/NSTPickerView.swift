40c1f3f26457f4b13fbe82a6da515642c8cb6837
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-03-11 23:33:26 +0900

    [Chore/#28] - 필요 없는 코드 제거

--- Noostak_iOS/Noostak_iOS/Global/Utility/NSTPickerView.swift
+++ Noostak_iOS/Noostak_iOS/Global/Utility/NSTPickerView.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 import Then
-import SnapKit
 
 import RxSwift
 import RxCocoa
@@ -27,12 +26,20 @@ final class NSTPickerView: UIPickerView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        delegate = self
-        dataSource = self
+        
+        setDelegate()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Method
+
+    private func setDelegate() {
+        delegate = self
+        dataSource = self
     }
 }
 
